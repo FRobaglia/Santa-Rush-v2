579df520fc3ae5babcfe5c45bff88f61359e12dd
unknown <felix.robaglia@gmail.com>
2019-03-05 20:15:35 +0100

    add fireplace random generation and santa gifts

--- js/main.js
+++ js/main.js
@@ -1,54 +1,46 @@
 let santa = {
   DOM: document.querySelector(".santa"),
-  currentJump: 0,
-  canJump: true,
-  reachedTop: false,
-  gravity: 5
+  jump: {
+    currentJump: 0,
+    canJump: true,
+    reachedTop: false
+  },
+  canUseGift: true,
+  lifes: 3
 };
 
-let initialGround = document.querySelector(".initial-ground");
-
+let fireplaces = [];
+let gifts = [];
 let keyIsPressed = {};
 
-window.addEventListener(
-  "keydown",
-  event => {
-    keyIsPressed[event.keyCode] = true;
-  },
-  true
-);
-window.addEventListener(
-  "keyup",
-  event => {
-    keyIsPressed[event.keyCode] = false;
-  },
-  true
-);
+let fireplaceGen;
+
+let initialGround = document.querySelector(".initial-ground");
+let game = document.querySelector(".game");
 
 function santaJump() {
-  if (keyIsPressed[38] && santa.canJump) {
-
-    if (!santa.reachedTop) {
-    santa.currentJump -= 12;
-    santa.DOM.style.transform = `translateY(${santa.currentJump}px)`;
+  if (keyIsPressed[38] && santa.jump.canJump) {
+    if (!santa.jump.reachedTop) {
+      santa.jump.currentJump -= 6;
+      santa.DOM.style.transform = `translateY(${santa.jump.currentJump}px)`;
     }
 
-    if (santa.currentJump < -300) {
-      santa.reachedTop = true;
+    if (santa.jump.currentJump < -300) {
+      santa.jump.reachedTop = true;
       setTimeout(() => {
-        santa.canJump = false;
-        santa.reachedTop = false;
-      }, 500);
-      santa.DOM.style.transform = `translateY(${santa.currentJump}px)`;
+        santa.jump.canJump = false;
+        santa.jump.reachedTop = false;
+      }, 300);
+      santa.DOM.style.transform = `translateY(${santa.jump.currentJump}px)`;
     }
   } else {
-    santa.reachedTop = false;
-    santa.canJump = false;
+    santa.jump.reachedTop = false;
+    santa.jump.canJump = false;
     if (!collisionBetween(santa.DOM, initialGround)) {
-      santa.currentJump += 8;
-      santa.DOM.style.transform = `translateY(${santa.currentJump}px)`;
+      santa.jump.currentJump += 4;
+      santa.DOM.style.transform = `translateY(${santa.jump.currentJump}px)`;
     } else {
-      santa.canJump = true;
+      santa.jump.canJump = true;
     }
   }
 }
@@ -79,12 +71,100 @@ function collisionBetween(el1, el2) {
   }
 }
 
+function fireplaceGeneration() {
+  let fireplace = {
+    DOM: document.createElement("div"),
+    translateX: 0
+  };
+  fireplace.DOM.classList.add("fireplace");
+  game.append(fireplace.DOM);
+
+  fireplaces.push(fireplace);
+
+  let timeNext = Math.random() * 2 + 1;
+  fireplaceGen = setTimeout(fireplaceGeneration, timeNext * 1000);
+}
+
+function moveLevel() {
+  for (let i = 0; i < fireplaces.length; i++) {
+    const fireplace = fireplaces[i];
+    fireplace.translateX -= 4;
+    fireplace.DOM.style.transform = `translateX(${fireplace.translateX}px)`;
+  }
+  for (let i = 0; i < gifts.length; i++) {
+    const gift = gifts[i];
+    gift.translateY += 4;
+    gift.DOM.style.transform = `translate(90px, ${gift.translateY}px)`;
+  }
+}
+
+document.addEventListener("keypress", function(event) {
+  if (event.keyCode === 32 && santa.canUseGift) {
+    santa.canUseGift = false;
+    let gift = {
+      DOM: document.createElement("div"),
+      translateY: getInfo(santa.DOM).y + 100
+    };
+    gift.DOM.classList.add("gift");
+    gift.DOM.style.transform = `translate(90px, ${gift.translateY}px)`;
+    game.append(gift.DOM);
+    gifts.push(gift);
+
+    setTimeout(() => {
+      santa.canUseGift = true;
+    }, 500);
+  }
+});
+
+function checkGiftCollsion() {
+  for (let i = 0; i < gifts.length; i++) {
+    const gift = gifts[i];
+    for (let j = 0; j < fireplaces.length; j++) {
+      const fireplace = fireplaces[j];
+      if (collisionBetween(gift.DOM, fireplace.DOM)) {
+        gift.DOM.remove();
+      } 
+      
+      if (collisionBetween(gift.DOM, initialGround)) {
+        gift.DOM.remove();
+        santa.lifes--;
+        if (santa.lifes <= 0) {
+          document.getElementById("crossThree").classList.add("isLost");
+          // gameOver();
+        } else if (santa.lifes === 2) {
+          document.getElementById("crossOne").classList.add("isLost");
+        } else if (santa.lifes === 1) {
+          document.getElementById("crossTwo").classList.add("isLost");
+        }
+      }
+    }
+  }
+}
+
 function init() {
   gameLoop();
+  fireplaceGeneration();
+
+  window.addEventListener(
+    "keydown",
+    event => {
+      keyIsPressed[event.keyCode] = true;
+    },
+    true
+  );
+  window.addEventListener(
+    "keyup",
+    event => {
+      keyIsPressed[event.keyCode] = false;
+    },
+    true
+  );
 }
 
 function gameLoop() {
+  checkGiftCollsion();
   santaJump();
+  moveLevel();
   setTimeout(gameLoop, 10);
 }
 
